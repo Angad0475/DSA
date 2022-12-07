f2732cd7f4684a6be4715f61f4168c0d9d3b120f
Angad0475 <94304532+Angad0475@users.noreply.github.com>
2022-12-07 20:16:13 +0530

    Add files via upload

---
+++ selectionsort.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#define n 5
+using namespace std;
+
+int a[n];
+int i;
+int j;
+int temp;
+
+void displayy()
+{
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
+void selectionsort()
+{
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[j]<a[i])
+            {
+                temp=a[j];
+                a[j]=a[i];
+                a[i]=temp;
+            }
+
+
+            
+        }
+    }
+
+
+}
+
+int main()
+{
+    for(i=0;i<n;i++)
+    {
+        scanf("%d ",&a[i]);
+    }
+
+
+    displayy();
+
+    printf("running selection sort\n");
+
+    selectionsort();
+
+    displayy();
+
+
+
+    return 0;
+} 
