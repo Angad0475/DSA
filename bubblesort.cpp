f3c129c74e8d017522852c474ebb7cf5eaa04296
Angad0475 <94304532+Angad0475@users.noreply.github.com>
2023-02-01 20:55:36 +0530

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+int a[50];
+
+int i;
+int j;
+int temp;
+int n;
+
+void bubblesort()
+{
+    for(i=1;i<=n-1;i++)
+    {
+        for(j=0;j<n-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+}
+
+void display(){
+    for(i=0;i<n;i++)
+    {
+        cout << a[i] << " ";
+    }
+
+}
+
+int main()
+{
+    cout<<"enter the size"<<endl;
+    cin >> n;
+    for(i=0;i<n;i++)
+    {
+        cin >> a[i];
+    }
+
+    cout << "running buuble  sort" << "\n";
+
+    bubblesort();
+
+    display();
+
+    return 0;
+
+}
