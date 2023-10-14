a6702ea2f970bfaaf972b08e82d13b1f240ce516
Angad0475 <94304532+Angad0475@users.noreply.github.com>
2023-10-14 14:02:30 +0530

    Update bubblesort.cpp

--- searching&sorting/bubblesort.cpp
+++ searching&sorting/bubblesort.cpp
@@ -1,51 +1,27 @@
-#include<iostream>
-using namespace std;
-int a[50];
-
-int i;
-int j;
-int temp;
-int n;
-
-void bubblesort()
+class Solution
 {
-    for(i=0;i<=n-1;i++)
+    public:
+    //Function to sort the array using bubble sort algorithm.
+    void bubbleSort(int arr[], int n)
     {
-        for(j=0;j<n-i-1;j++)
+        int i;
+        int j;
+        int temp;
+        
+        for(i=0;i<=n-1;i++)
         {
-            if(a[j]>a[j+1])
+            for(j=0;j<n-i-1;j++)
             {
-                temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
+                if(arr[j]>arr[j+1])
+                {
+                    temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    
+                }
             }
         }
+        
     }
-}
-
-void display(){
-    for(i=0;i<n;i++)
-    {
-        cout << a[i] << " ";
-    }
-
-}
-
-int main()
-{
-    cout<<"enter the size"<<endl;
-    cin >> n;
-    for(i=0;i<n;i++)
-    {
-        cin >> a[i];
-    }
-
-    cout << "running buuble  sort" << "\n";
-
-    bubblesort();
-
-    display();
-
-    return 0;
-
-}
+    
+};  
