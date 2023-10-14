19f508f226e13dcc2f06e80a86c51c7893cca550
Angad0475 <94304532+Angad0475@users.noreply.github.com>
2023-10-14 14:11:49 +0530

    Update selectionsort.cpp

--- searching&sorting/selectionsort.cpp
+++ searching&sorting/selectionsort.cpp
@@ -1,55 +1,22 @@
-#include<iostream>
-#define n 5
-using namespace std;
-
-int i;
-int j;
-int a[n];
-int temp;
-int indexofmin;
-indexofmin=a[i];
-
-void printarray()
+class Solution
 {
-    for(i=0;i<n;i++)
+    public:
+    
+ 
+    void selectionSort(int arr[], int n)
     {
-        cout << a[i]<<endl;
+       int i;
+       int j;
+       int temp;
+       
+       for(i=0;i<n-1;i++){
+           for(j=i+1;j<n;j++){
+               if(arr[j]<arr[i]){
+                   temp=arr[j];
+                   arr[i]=arr[j];
+                   arr[i]=temp;
+               }
+           }
+       }
     }
-}
-void selectionsort()
-{
-    for(i=0;i<n-1;i++)
-    {
-        for(j=i+1;j<n;j++)
-        {
-            if(a[j] < a[i])//a[i] is the index of minimum value 
-            {
-            temp=a[j];
-            a[j]=a[i];
-            a[i]=temp;
-            }
-
-        }
-    }
-}
-
-int main()
-{
-    cout << "enter the elements "<<endl;
-    for(i=0;i<=n-1;i++)
-    {   
-        cin >> a[i];
-    }
-
-    cout << "running selection sort "<<endl;
-
-    selectionsort();
-
-    cout << "the sorted elemnts are "<<endl;
-
-    printarray();
-
-
-    return 0;
-
-}
+};
