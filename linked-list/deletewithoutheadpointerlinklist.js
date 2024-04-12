d39839fbb0f017e749428c99295273c481f057c0
Angad Singh <94304532+Angad0475@users.noreply.github.com>
2024-04-12 17:35:30 +0530

    Update deletewithoutheadpointerlinklist.js

--- linked-list/deletewithoutheadpointerlinklist.js
+++ linked-list/deletewithoutheadpointerlinklist.js
@@ -6,13 +6,16 @@ var deleteNode = function(node) {
     else{
         if(node.next!==null){     
     var prev= node;
-     var curr= node.next;
+     var curr= node.next;  
      var nxt_nxt= node.next.next;
 
-     prev.val= curr.val;
-     prev.next=nxt_nxt;
-    }
-    }
+     prev.val= curr.val;    
+     prev.next=nxt_nxt; 
+      }
+}
+     //The most efficient approach to deleting a node without the head pointer involves changing 
+      //the current node’s value to the value of the next node and updating the next pointer
+     //of the current node to its next's next node. 
 
      
 };
