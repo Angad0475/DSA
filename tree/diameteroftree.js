a133af3f1b68cfdac53b734bddbeaefe0c81374b
Angad Singh <94304532+Angad0475@users.noreply.github.com>
2024-05-05 00:38:28 +0530

    Update and rename diameteroftree.cpp to diameteroftree.js

---
+++ tree/diameteroftree.js
@@ -0,0 +1,23 @@
+var diameterOfBinryTree =function(root) {
+ if(!root){
+   return 0;
+ }
+
+let maxDiameter =0 ;
+const dfs =(node)=>{
+ if(!node){
+       return 0;
+ }
+let left= dfs(node.left);
+let right =dfs(node.right);
+
+maxDiameter =Math.max(maxDiameter,left+right);
+
+return 1+ Math.max(left,right);
+}
+
+
+dfs(root);
+
+return maxDiameter;
+  
