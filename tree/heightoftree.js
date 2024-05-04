b14051470db9a90c350ca15d86cbed7f396b707d
Angad Singh <94304532+Angad0475@users.noreply.github.com>
2024-05-04 20:33:54 +0530

    Update heightoftree.js

--- tree/heightoftree.js
+++ tree/heightoftree.js
@@ -4,17 +4,17 @@ this.val=val;
 this.left=null;
 this.right=null;
    }
-}//
+}//             //a node containing root value and left child and rght child.
 
 
 var maxDepth = function(root) {
     if (root===null){
         return 0;
     }
-    var leftSubtree= maxDepth(root.left);
-    var rightSubtree =maxDepth(root.right);
+    var leftSubtree= maxDepth(root.left); //recusrsivly go tohe leftmost leaf node.
+    var rightSubtree =maxDepth(root.right);//recursivly go to the rightmost leaf node.
 
-    var result = Math.max(leftSubtree,rightSubtree)+1;
+    var result = Math.max(leftSubtree,rightSubtree)+1; // get the max of left node and right node and add the root node(+1).
 
-    return result;
+    return result; //return the result .
 };
