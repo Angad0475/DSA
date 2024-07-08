de0c328122d9def4589c6944c7f39f603232ab0a
Angad Singh <94304532+Angad0475@users.noreply.github.com>
2024-07-08 12:16:50 +0530

    Update diameteroftree.js

--- tree/diameteroftree.js
+++ tree/diameteroftree.js
@@ -1,24 +1,23 @@
-var diameterOfBinryTree =function(root) {
- if(!root){
-   return 0;
- }
+var diameterOfBinaryTree = function(root) {
+     if(!root){
+        return 0;
+     }
+     let maxDiameter =0;
 
-let maxDiameter =0 ;
-const dfs =(node)=>{
- if(!node){
-       return 0;
- }
-let left= dfs(node.left);
-let right =dfs(node.right);
+     const dfs =(node)=>{
+        if(!node){
+            return 0;
+        }
+        let left = dfs(node.left);
+        let right =dfs(node.right);
 
-maxDiameter =Math.max(maxDiameter,left+right);
+        maxDiameter= Math.max(maxDiameter,left+right);
 
-return 1+ Math.max(left,right);
-}
+        return Math.max(left,right) + 1;
+     }
+     dfs(root);
 
+     return maxDiameter;
 
-dfs(root);
+};
 
-return maxDiameter;
-}
-  
