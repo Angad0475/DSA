720bc55967c7622f4dcdbba7a978754c0e05bd38
Angad Singh <94304532+Angad0475@users.noreply.github.com>
2024-07-09 14:10:24 +0530

    Update and rename rightview.cpp to rightview.js

---
+++ tree/rightview.js
@@ -0,0 +1,19 @@
+function solve(root,level,res){
+    if(!root){
+        return;
+    }
+    if(res.length===level){
+        res.push(root.val)
+    }
+    solve(root.right,level+1,res);
+    solve(root.left,level+1,res);
+}
+
+
+
+var rightSideView = function(root) {
+    var res=[];
+    solve(root,0,res);
+    return res;
+
+}; 
