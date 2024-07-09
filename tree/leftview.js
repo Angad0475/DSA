ca8ce4978eb6bf00073e37d6e36b46197af4e71c
Angad Singh <94304532+Angad0475@users.noreply.github.com>
2024-07-09 21:13:09 +0530

    Update leftview.js

--- tree/leftview.js
+++ tree/leftview.js
@@ -5,13 +5,13 @@ function solve(root,level,res){
     if(res.length===level){
         res.push(root.val)
     }
-    solve(root.right,level+1,res);
     solve(root.left,level+1,res);
+    solve(root.right,level+1,res);
 }
 
 
 
-var rightSideView = function(root) {
+var leftSideView = function(root) {
     var res=[];
     solve(root,0,res);
     return res;
