73d101bd1601099abd4125608b5c100d12f2e991
Angad Singh <94304532+Angad0475@users.noreply.github.com>
2024-08-25 18:56:17 +0530

    Update TwoSum.js

--- array/TwoSum.js
+++ array/TwoSum.js
@@ -2,8 +2,8 @@ const twosum= function(nums,target){
     let map = new Map();
     for(let i=0;i< nums.length;i++){
         let targetValue=target-nums[i];
-        if(map.has(targetValue)){
-            return (map.get(targetValue),i);
+        if(map.has(targetValue)){ //has check whether the key is present in the map .
+            return (map.get(targetValue),i); //gets the key and return the valueof that key .
         }
         map.set(nums[i],i);
         
