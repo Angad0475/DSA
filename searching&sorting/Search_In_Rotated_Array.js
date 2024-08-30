a2d4937274086c5566b176ecfd33ed25ba86e3ea
Angad Singh <94304532+Angad0475@users.noreply.github.com>
2024-08-30 13:51:52 +0530

    Update Search_In_Rotated_Array.js

--- searching&sorting/Search_In_Rotated_Array.js
+++ searching&sorting/Search_In_Rotated_Array.js
@@ -1,34 +1,27 @@
 var search = function(nums, target) {
-    let low=0, high=nums.length-1;
-    while(low<=high){
-        let mid= low+ Math.floor((high-low)/2);
-
-        if(nums[mid]===target){
+    let low = 0, high = nums.length - 1;
+    
+    while (low <= high) {
+        let mid = low + Math.floor((high - low) / 2);
+        
+        if (nums[mid] === target) {
             return mid;
         }
-
-        if(nums[low]<=nums[mid]){ // if left side sorted 
-            if(nums[low]<=target && target < nums[mid]){ //check is target is located in left side.
-                high=mid-1;
+        
+        if (nums[low] <= nums[mid]) { // if the left side is sorted
+            if (nums[low] <= target && target < nums[mid]) { // check if target is in the left side
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
-            else{
-                low=mid+1;
+        } else { // if the left side is not sorted, the right side must be sorted
+            if (nums[mid] < target && target <= nums[high]) { // check if target is in the right side
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
-        }
-        else{ // if left side is not srted then right side is sorted then we will directly check wether element is present or not in the righgt side.
-            if(nums[mid]<taget && target <=nums[high]){
-                low=mid+1;
-            }
-            else{
-                high= mid-1;
-            }
-
         }
     }
+    
     return -1;
-
 };
-
-
-
-
