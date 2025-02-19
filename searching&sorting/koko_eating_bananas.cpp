2f85b096123c5b2b8ca52eda6715a1045e7a8955
Angad Singh <94304532+Angad0475@users.noreply.github.com>
2025-02-19 14:12:03 +0530

    Create koko_eating_bananas.cpp

---
+++ searching&sorting/koko_eating_bananas.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    int FindMax(vector<int>& v) {
+        int n = v.size();
+        int MAXI = INT_MIN;
+
+        for (int i = 0; i < n; i++) {
+            MAXI = max(MAXI, v[i]);
+        }
+        return MAXI;
+    }
+
+    int requiredHours(vector<int>& v, int hourly) {
+        int totalHours = 0;
+        int n = v.size();
+        for (int i = 0; i < n; i++) {
+            totalHours += ceil((double)v[i] / (double)hourly);
+        }
+        return totalHours;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1, high = FindMax(piles);
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int totalH = requiredHours(piles, mid); // Fixed variable name
+
+            if (totalH <= h) {
+                high = mid - 1;
+            } else {
+                low = mid + 1; 
+            }
+        }
+        return low;
+    }
+};
